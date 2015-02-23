aa9460b6b57cf5882c31d222c64113fc6b2888a2
1306794 <1306794@RG43032.rgu.ac.uk>
2015-02-23 10:46:15 +0000

    Copy done.

---
+++ Repo1/src/repo1/Dictionary.java
@@ -0,0 +1,21 @@
+package repo1;
+
+
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author 1302435
+ * @version 23/02/2015
+ */
+public interface Dictionary {
+    
+    public String getDefinition(String str);
+      
+    
+}

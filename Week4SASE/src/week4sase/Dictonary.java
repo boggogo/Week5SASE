4d0e746cf323c3438f36ba59b78044bb0558a5f6
1306794 <1306794@RG43032.rgu.ac.uk>
2015-02-23 09:31:07 +0000

    new commit

---
+++ Week4SASE/src/week4sase/Dictonary.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week4sase;
+
+/**
+ *
+ * @author 1306794
+ */
+public interface Dictonary {
+
+    public String getDefinition(String str);
+
+}
---
+++ Week4SASE/src/week4sase/Tester.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week4sase;
+import java.lang.annotation.*;
+/**
+ *
+ * @author 1306794
+ */
+
+
+public class Tester {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+    }
+    
+}

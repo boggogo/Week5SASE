1eb46452e4d46c8a838a79f52d23a17cf97ea0bd
1306794 <1306794@RG43032.rgu.ac.uk>
2015-02-23 10:48:32 +0000

    Copy 2 done.

---
+++ Repo2/src/repo2/GeneralDictionary.java
@@ -0,0 +1,33 @@
+package repo2;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+/**
+ *
+ * @author 1306794
+ */
+public class GeneralDictionary implements Dictionary{
+    private SortedMap<String, String>map;
+
+    public GeneralDictionary() {
+        map = new TreeMap<String, String>();
+        map.put("book", "a set of printed pages, bound with a cover");
+        map.put("editor", "a person who edits");
+    }
+    
+    
+    
+    @Override
+    public String getDefinition(String str) {
+        return map.get(str);
+    }
+    
+}
